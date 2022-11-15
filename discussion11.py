466f73de0f4b673f88235dcd1f96d4a4f979e382
Zeinab Ghandour <zeinabghandour@Zeinabs-MacBook-Air-2.local>
2022-11-15 16:44:59 -0500

    did some stuff

--- discussion11.py
+++ discussion11.py
@@ -39,13 +39,16 @@ def create_species_table(cur, conn):
 # TASK 1
 # CREATE TABLE FOR PATIENTS IN DATABASE
 def create_patients_table(cur, conn):
-    pass
-
+    cur.execute("DROP TABLE IF EXISTS Patients")
+    cur.execute("CREATE TABLE Patients (pet_id INTEGER PRIMARY KEY,name TEXT,\
+        species_id INTEGER, age INTEGER, cuteness INTEGER, aggressiveness INTEGER)")
+    conn.commit()
 
 # ADD FLUFFLE TO THE TABLE
 def add_fluffle(cur, conn):
-    pass
-    
+     cur.execute("INSERT INTO Patients (pet_id ,name,\
+        species_id , age, cuteness, aggressiveness)VALUES(?,?,?,?,?,?)",("0","Fluffie","0","3","90","100"))
+     conn.commit()
 
 # TASK 2
 # CODE TO ADD JSON TO THE TABLE
@@ -57,9 +60,11 @@ def add_pets_from_json(filename, cur, conn):
     file_data = f.read()
     f.close()
     json_data = json.loads(file_data)
+    print(json_data)
 
     # THE REST IS UP TO YOU
-    pass
+
+    
 
 
 # TASK 3
